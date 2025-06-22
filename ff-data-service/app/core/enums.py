ad4b7da458fff2c7c3332b0ae965bd2d6effa1a7
Roderick Lan <rodericklan03@gmail.com>
2025-06-22 15:19:13 -0600

    team enum

--- ff-data-service/app/core/enums.py
+++ ff-data-service/app/core/enums.py
@@ -2,13 +2,14 @@
 
 from enum import Enum
 
+
 class PlayerStatus(Enum):
     H = "H"
     O = "O"
     IR = "IR"
     NA = "NA"
-    
-    
+
+
 class Position(Enum):
     QB = "QB"
     WR = "WR"
@@ -16,3 +17,38 @@ class Position(Enum):
     TE = "TE"
     K = "K"
     DST = "D/ST"
+
+
+class Team(Enum):
+    ARI = "ARI"
+    ATL = "ATL"
+    BAL = "BAL"
+    BUF = "BUF"
+    CAR = "CAR"
+    CHI = "CHI"
+    CIN = "CIN"
+    CLE = "CLE"
+    DAL = "DAL"
+    DEN = "DEN"
+    DET = "DET"
+    GB = "GB"
+    HOU = "HOU"
+    IND = "IND"
+    JAX = "JAX"
+    KC = "KC"
+    LV = "LV"
+    LAC = "LAC"
+    LAR = "LAR"
+    MIA = "MIA"
+    MIN = "MIN"
+    NE = "NE"
+    NO = "NO"
+    NYG = "NYG"
+    NYJ = "NYJ"
+    PHI = "PHI"
+    PIT = "PIT"
+    SF = "SF"
+    SEA = "SEA"
+    TB = "TB"
+    TEN = "TEN"
+    WAS = "WAS"
