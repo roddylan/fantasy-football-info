5060e12ffba992f6ec632eeaa2d92cbc54575d96
Roderick Lan <rodericklan03@gmail.com>
2025-06-22 15:30:30 -0600

    init_db function

--- ff-data-service/app/core/db.py
+++ ff-data-service/app/core/db.py
@@ -3,6 +3,7 @@
 from app.core.config import settings
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from app.models.base import Base
 
 db_url = settings.db_url
 
@@ -11,3 +12,5 @@ engine = create_engine(db_url)
 
 SessionLocal = sessionmaker(engine)
 
+def init_db():
+    Base.metadata.create_all(engine)
