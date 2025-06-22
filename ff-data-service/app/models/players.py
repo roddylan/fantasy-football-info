ef085d08b7a42f4a205f91f88855efecc7404833
Roderick Lan <rodericklan03@gmail.com>
2025-06-22 16:06:44 -0600

    added player team table

--- ff-data-service/app/models/players.py
+++ ff-data-service/app/models/players.py
@@ -4,23 +4,31 @@ from app.models.base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Enum, Integer, ForeignKey
 from typing import List
-from app.core.enums import Position, PlayerStatus
+from app.core import enums
 
 class Player(Base):
     __tablename__ = "player"
     
     id: Mapped[str] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
-    positions: Mapped[List[Position]] = mapped_column(List[Enum(Position)])
+    positions: Mapped[List[enums.Position]] = mapped_column(List[Enum(enums.Position)])
     
 
-class PlayerStatusModel(Base):
+class PlayerStatus(Base):
     __tablename__ = 'player_status'
     player_id: Mapped[str] = mapped_column(ForeignKey("player.id"), primary_key=True)
     year: Mapped[int] = mapped_column(primary_key=True)
     week: Mapped[int] = mapped_column(primary_key=True)
-    status: Mapped[PlayerStatus] = mapped_column(Enum(PlayerStatus))
+    status: Mapped[enums.PlayerStatus] = mapped_column(Enum(enums.PlayerStatus))
     
     
     player_id = relationship("Player", primary_key=True)
+    
+    
+class PlayerTeam(Base):
+    __tablename__ = 'player_team'
+    player_id: Mapped[str] = mapped_column(ForeignKey("player.id"), primary_key=True)
+    year: Mapped[int] = mapped_column(primary_key=True)
+    week: Mapped[int] = mapped_column(primary_key=True)
+    team: Mapped[enums.Team] = mapped_column(Enum(enums.Team))
     
