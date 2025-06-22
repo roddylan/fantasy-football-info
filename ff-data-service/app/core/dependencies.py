a1fe7f4715e138f238e15906570c545c31680c59
Roderick Lan <rodericklan03@gmail.com>
2025-06-22 15:26:50 -0600

    get_db function for dep inj

--- ff-data-service/app/core/dependencies.py
+++ ff-data-service/app/core/dependencies.py
@@ -1,6 +1,14 @@
 # app/core/dependencies.py
 from app.core.config import settings, Settings
+from app.core.db import SessionLocal
+
 
 def get_settings() -> Settings:
     return settings
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
