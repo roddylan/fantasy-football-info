cbbfd38e42f71de6152138b90ed38456b31fc0c8
Roderick Lan <rodericklan03@gmail.com>
2025-06-22 16:40:17 -0600

    fill players pretty much done, created fill_player endpoint for status data, etc.

--- ff-data-service/app/api/players.py
+++ ff-data-service/app/api/players.py
@@ -3,6 +3,9 @@
 from fastapi import APIRouter, Depends
 from app.core.dependencies import get_db
 from sqlalchemy.orm import Session
+from app.services.player_info import PlayerInfoService
+from typing import Optional
+from fastapi import Response
 
 router = APIRouter()
 
@@ -12,6 +15,13 @@ def get_all_players():
     return {'msg': 'success'}
 
 
-@router.post('/fill_all')
+@router.post('/fill_players')
 def fill_players(db: Session = Depends(get_db)):
+    service = PlayerInfoService()
+    result = service.fill_players_table(db=db)
+    return Response(status_code=result)
+        
+
+@router.post('/fill_player')
+def fill_players(player_name: str, db: Session = Depends(get_db)):
     pass
--- ff-data-service/app/services/player_info.py
+++ ff-data-service/app/services/player_info.py
@@ -1,11 +1,12 @@
 # app/services/player_info.py
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 import requests
 from typing import Dict, Tuple, List
 from bs4 import BeautifulSoup, Tag
 from app.models.players import Player
 from app.core import enums
-
+from fastapi import status
 
 class PlayerInfoService:
     def __init__(self):
@@ -15,29 +16,37 @@ class PlayerInfoService:
         assert len(letter) > 0
         return f"https://www.pro-football-reference.com/players/{letter[0].upper()}/"
 
-    def fill_players(self, db: Session, year: int = 2024) -> None:
-        for i in range(0, 26):
-            letter = chr(ord("a") + i)
-            res = requests.get(self._pfr_web(letter.upper()))
-            if res.status_code != 200:
-                continue
-            html = BeautifulSoup(res.text)
-            links: List[Tag] = html.find("div", {"id": "all_players"}).find_all("a")
-            # res.close()
-            for link in links:
-                player_name = link.text
-                player_id = link.get("href").split("/")[-1].split(".")[0]
-                active = link.parent.name == "b"
-                pos = res.findall(r"\((.*?)\)", link.next_sibling.strip())[0]
+    def fill_players_table(self, db: Session, year: int = 2024) -> None:
+        try:
+            for i in range(0, 26):
+                letter = chr(ord("a") + i)
+                res = requests.get(self._pfr_web(letter.upper()))
+                if res.status_code != 200:
+                    continue
+                html = BeautifulSoup(res.text)
+                links: List[Tag] = html.find("div", {"id": "all_players"}).find_all("a")
+                # res.close()
+                for link in links:
+                    player_name = link.text
+                    player_id = link.get("href").split("/")[-1].split(".")[0]
+                    # active = link.parent.name == "b"
+                    pos = res.findall(r"\((.*?)\)", link.next_sibling.strip())[0]
 
-                try:
-                    player = Player(
-                        id=player_id,
-                        name=player_name,
-                        positions=[enums.Position(pos)]
-                    )
-                    db.add(player)
-                    db.commit()
-                except ValueError:
-                    # invalid position, non offensive player
-                    
+                    try:
+                        player = Player(
+                            id=player_id,
+                            name=player_name,
+                            positions=[enums.Position(pos)]
+                        )
+                        db.add(player)
+                        db.commit()
+                    except ValueError:
+                        # invalid position, non offensive player
+                        # skip player
+                        continue
+                    except IntegrityError:
+                        db.rollback()
+            return status.HTTP_200_OK
+        except Exception:
+            return status.HTTP_500_INTERNAL_SERVER_ERROR
+        
