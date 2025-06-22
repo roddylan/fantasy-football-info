f3a7908439db9ddb6c508eb6da9430a9b6be956e
Roderick Lan <rodericklan03@gmail.com>
2025-06-22 15:33:38 -0600

    added db host to settings

--- ff-data-service/app/core/config.py
+++ ff-data-service/app/core/config.py
@@ -6,9 +6,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     db_user: str
     db_password: str
+    db_host: str
     
     
     @property
     def db_url(self):
-        return f'postgres://{self.db_user}:{self.db_password}@db/football_info'
+        return f'postgres://{self.db_user}:{self.db_password}@{self.db_host}/football_info'
 settings = Settings()
