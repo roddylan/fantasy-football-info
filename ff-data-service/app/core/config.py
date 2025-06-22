2783dacc4996a8dda9baf08e59a2e0c24b030650
Roderick Lan <rodericklan03@gmail.com>
2025-06-22 15:22:56 -0600

    added db url to settings

--- ff-data-service/app/core/config.py
+++ ff-data-service/app/core/config.py
@@ -7,4 +7,8 @@ class Settings(BaseSettings):
     db_user: str
     db_password: str
     
+    
+    @property
+    def db_url(self):
+        return f'postgres://{self.db_user}:{self.db_password}@db/football_info'
 settings = Settings()
