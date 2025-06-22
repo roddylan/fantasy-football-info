4f006b545f7f5cb14afc869ffd6dcae4e307e5bb
Roderick Lan <rodericklan03@gmail.com>
2025-06-22 15:11:44 -0600

    PlayerStatus enum

--- ff-data-service/app/core/enums.py
+++ ff-data-service/app/core/enums.py
@@ -1,3 +1,11 @@
 # app/core/enums.py
 
 from enum import Enum
+
+class PlayerStatus(Enum):
+    H = "H"
+    O = "O"
+    IR = "IR"
+    NA = "NA"
+    
+    
--- ff-data-service/app/schema/players.py
+++ ff-data-service/app/schema/players.py
@@ -2,10 +2,11 @@
 
 from pydantic import BaseModel
 from typing import List
+from app.core.enums import PlayerStatus
 
 class Player(BaseModel):
     name: str
     pff_id: str
     team: str
-    status: str
+    status: PlayerStatus
     
