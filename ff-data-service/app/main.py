d33cbfcb07cf21423c3a9d68a3ae17357f13c53e
Roderick Lan <rodericklan03@gmail.com>
2025-06-22 15:32:20 -0600

    created lifespan function

--- ff-data-service/app/main.py
+++ ff-data-service/app/main.py
@@ -1,9 +1,16 @@
 # app/main.py
 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.api.router import router as api_router
+from app.core.db import init_db
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(api_router, '')
